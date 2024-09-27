1fb104d692dba30c8705c5a3e3e62197c38f2486
sharai-pi211 <kate15052004@gmail.com>
2024-09-28 02:13:47 +0300

    feat: add contacnts page and outlet

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,49 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from "./pages/Main";
+import Contact from "./components/Contact";
+import ContactsList from "./components/ContactsList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main/>,
+  },
+  {
+    path: "/contacts",
+    element: <ContactsList/>,    
+    children: [
+      {
+        path: ":contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn/>,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp/>,
+  },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
 
